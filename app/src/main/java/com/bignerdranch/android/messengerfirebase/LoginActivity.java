5b49e19df0ea40797846fc0cbb55761864786e55
Aleksey <meysonsib200189@mail.ru>
2024-03-13 13:58:16 +0700

    add LoginViewModel выскакивает Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'androidx.lifecycle.LiveData com.bignerdranch.android.messengerfirebase.LoginViewModel.getError()' on a null object reference                                        at com.bignerdranch.android.messengerfirebase.LoginActivity.observeViewModel(LoginActivity.java:79)                                        at com.bignerdranch.android.messengerfirebase.LoginActivity.onCreate(LoginActivity.java:38)

--- app/src/main/java/com/bignerdranch/android/messengerfirebase/LoginActivity.java
+++ app/src/main/java/com/bignerdranch/android/messengerfirebase/LoginActivity.java
@@ -2,14 +2,24 @@ package com.bignerdranch.android.messengerfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    static final String TAG_LOGIN = "LoginActivity";
+
+    private LoginViewModel viewModel;
 
     private EditText editTextEmail;
     private EditText editTextPassword;
@@ -25,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         initViews();
         onClickBtnLogin();
         onClickTextView();
+        observeViewModel();
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
     }
 
     private void initViews() {
@@ -41,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
-                //login
+                viewModel.login(email, password);
             }
         });
     }
@@ -50,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
         textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = ResetActivity.newIntent(LoginActivity.this,editTextEmail.getText().toString().trim()); //передаю почту из поля ввода
+                Intent intent = ResetActivity.newIntent(LoginActivity.this, editTextEmail.getText().toString().trim()); //передаю почту из поля ввода
                 startActivity(intent);
             }
         });
@@ -59,6 +71,32 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = RegistrationActivity.newIntent(LoginActivity.this);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }else {
+                    Log.d(TAG_LOGIN, errorMessage);
+                }
+            }
+        });
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Intent intent = UsersActivity.newIntent(LoginActivity.this);
+                    startActivity(intent);
+                }else {
+                    Intent intent = UsersActivity.newIntent(LoginActivity.this);
+                    startActivity(intent);
+                    Log.d(TAG_LOGIN,"тут пусто");
+                }
             }
         });
     }
---
+++ app/src/main/java/com/bignerdranch/android/messengerfirebase/LoginViewModel.java
@@ -0,0 +1,59 @@
+package com.bignerdranch.android.messengerfirebase;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginViewModel extends AndroidViewModel {
+
+    private FirebaseAuth auth;
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    private MutableLiveData<String> error = new MutableLiveData<>();
+
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+        auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    user.setValue(firebaseAuth.getCurrentUser());
+                }
+            }
+        });
+    }
+
+    public void login(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                error.setValue(e.getMessage());
+            }
+        });
+    }
+}
